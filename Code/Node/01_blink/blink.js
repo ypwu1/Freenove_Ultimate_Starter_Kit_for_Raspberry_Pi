7537ee63404e9a58ce2b77a2f589bc38268c44bd
oscar wu <oscar.wu@latitudefinancial.com>
2019-08-18 17:04:51 +1000

    udpate blink.js

--- Code/Node/01_blink/blink.js
+++ Code/Node/01_blink/blink.js
@@ -4,6 +4,10 @@ const Gpio = require('onoff').Gpio; // Gpio class
 const led = new Gpio(11, 'out');       // Export GPIO17 as an output
 let stopBlinking = false;
 
+if (!led.accessible) {
+    throw "gpio is not available!"
+}
+
 // Toggle the state of the LED connected to GPIO17 every 200ms
 const blinkLed = () => {
     if (stopBlinking) {
