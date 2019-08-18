238d509d587c99db7c12691aef90b53f15ca082f
oscar wu <oscar.wu@latitudefinancial.com>
2019-08-18 16:54:04 +1000

    blink in js

---
+++ Code/Node/01_blink/blink.js
@@ -0,0 +1,22 @@
+#!/bin/usr/local/node
+
+const Gpio = require('onoff').Gpio; // Gpio class
+const led = new Gpio(11, 'out');       // Export GPIO17 as an output
+let stopBlinking = false;
+
+// Toggle the state of the LED connected to GPIO17 every 200ms
+const blinkLed = () => {
+    if (stopBlinking) {
+        return led.unexport();
+    }
+
+    led.read()
+        .then(value => led.write(value ^ 1))
+        .then(_ => setTimeout(blinkLed, 200))
+        .catch(err => console.log(err));
+};
+
+blinkLed();
+
+// Stop blinking the LED after 5 seconds
+setTimeout(_ => stopBlinking = true, 5000);
